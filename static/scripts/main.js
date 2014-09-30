5dc8d0342b3aaa0734bb553ec06ada9afb9e5c7f
Kenji Pa <limouren@gmail.com>
2014-09-30 14:40:39 +0800

    parse KML into a json and render HTML w.r.t it

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -21,29 +21,86 @@
     return $.get(KML_URL);
   }
 
+  function parseData(data) {
+    var json = {},
+        name = data.attr('name'),
+        value = data.find('>value').text();
+
+    json.name = name;
+    json.value = value;
+
+    return json;
+  }
+
+  function parseExtendedData(data) {
+    var json = {},
+        datas = data.find('>Data');
+
+    $.each(datas, function(i, data) {
+      var d = parseData($(data));
+      json[d.name] = d.value;
+    });
+
+    return json;
+  }
+
+
+  function parsePlacemark(placemark) {
+    var json = {},
+        nameText = placemark.find('>name').text(),
+        extendedData = placemark.find('>ExtendedData');
+
+    json.name = nameText;
+    json.data = parseExtendedData(extendedData);
+
+    return json;
+  }
+
+  function parseFolder(folder) {
+    var json = {},
+        nameText = folder.find('>name').text(),
+        placemarks = folder.find('>Placemark');
+
+    json.name = nameText;
+    json.placemarks = [];
+
+    $.each(placemarks, function(i, placemark) {
+      var p = parsePlacemark($(placemark));
+      json.placemarks.push(p)
+    });
+
+    return json;
+  }
+
+  function parseDocument(doc) {
+    var json = {},
+        name = doc.find('>name').text(),
+        folders = doc.find('>Folder');
+
+    json.name = name;
+    json.folderDict = {};
+
+    $.each(folders, function(i, folder) {
+      var f = parseFolder($(folder));
+      json.folderDict[f.name] = f;
+    });
+
+    return json;
+  }
+
+  function parseKMLAsJson(data) {
+    var $kml = $(data),
+        doc = $kml.find('kml>Document');
+
+    return parseDocument(doc);
+  }
+
   function parseKML(data) {
+    var json = {};
     return $(data);
   }
 
-  function createFolderDict(folders) {
-    var d = {};
-    folders.each(function() {
-      var folder = $(this),
-          name = folder.find('>name').text();
-      d[name] = folder;
-    })
-
-    return d;
-  }
-
-  function renderExtendedDataItem(data) {
-    var name = data.attr('name'),
-        value = data.find('>value').text();
-
-    if (!name || !value || $.inArray(name, BLACKLIST_EXTENDED_DATA_NAMES) !== -1) {
-      return $();
-    }
-
+  function renderExtendedDataItem(name, value) {
     var $el = $('<li><span class="value-span"></span></li>'),
         nameSpan = $el.find('.data-name'),
         valueSpan = $el.find('.value-span');
@@ -53,10 +110,19 @@
     return $el;
   }
 
-  function renderPlacemark(placemark) {
-    var nameText = placemark.find('>name').text(),
-        datas = placemark.find('ExtendedData>Data');
+  function renderExtendedData(data) {
+    var $el = $();
 
+    $.each(data, function(key, value) {
+      if (key && value && $.inArray(key, BLACKLIST_EXTENDED_DATA_NAMES) !== -1) {
+        $el.append(renderExtendedDataItem(key, value));
+      }
+    });
+
+    return $el;
+  }
+
+  function renderPlacemark(placemark) {
     var $el = $(
       '<li><span class="name"></span>' +
         '<ul class="placemark">' +
@@ -65,30 +131,21 @@
       placemarkList = $el.find('.placemark'),
       nameSpan = $el.find('.name');
 
-    nameSpan.html(nameText);
-
-    datas.each(function() {
-      var data = $(this),
-          dataEl = renderExtendedDataItem(data);
-      placemarkList.append(dataEl);
-    });
+    nameSpan.html(placemark.name);
+    placemarkList.append(renderExtendedData(placemark.data));
 
     return $el;
   }
 
   function renderFolder(folder) {
-    var nameText = folder.find('>name').text(),
-        placemarks = folder.find('>Placemark');
-
     var $el = $('<h2 class="folder-name"></h2><ul class="placemark-list"></ul>'),
         nameEl = $el.filter('.folder-name'),
         placemarkList = $el.filter('.placemark-list');
 
-    nameEl.html(nameText);
+    nameEl.html(folder.name);
 
-    placemarks.each(function() {
-      var placemark = $(this),
-          $itemEl = renderPlacemark(placemark);
+    $.each(folder.placemarks, function(i, placemark) {
+      var $itemEl = renderPlacemark(placemark);
       placemarkList.append($itemEl);
     });
 
@@ -123,9 +180,7 @@
   }
 
   function renderDocument(doc) {
-    var folders = doc.find('Document>Folder');
-
-    FOLDER_DICT = createFolderDict(folders);
+    FOLDER_DICT = doc.folderDict;
 
     FolderSelect = renderFolderSelect();
     FolderSelect.on('change', handleFolderSelectChanged);
@@ -145,7 +200,7 @@
     FolderContainer = $('#content>.folder');
 
     fetchKML().then(function(data) {
-      var doc = parseKML(data);
+      var doc = parseKMLAsJson(data);
       renderDocument(doc);
 
       $(document.body).removeClass('loading');
